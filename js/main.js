60652c341c5a71ed4427a82309b21f5d869dddbc
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-21 15:50:23 -0700

    bars

--- js/main.js
+++ js/main.js
@@ -4,26 +4,24 @@ window.onload = function() {
     cvs.height = window.innerHeight;
     var painter = new Painter(cvs);
     var painterPath = new Painter(document.querySelector("#Canvas1"), false);
-    var slider1 = document.getElementById("m1");
-    var slider2 = document.getElementById("m2");
-    var slider3 = document.getElementById("m2");
+    var sliders = [document.getElementById("m1"), document.getElementById("m2"), document.getElementById("m3")];
     var masses = [];
     
     
     // Update the current slider value (each time you drag the slider handle)
-    slider1.oninput = function() {
+    sliders[0].oninput = function() {
         if (masses.length > 0) {
             masses[0].mass = this.value;
             painter.draw();
         }
     }
-    slider2.oninput = function() {
+    sliders[1].oninput = function() {
         if (masses.length > 1) {
             masses[1].mass = this.value;
             painter.draw();
         }
     }
-    slider3.oninput = function() {
+    sliders[2].oninput = function() {
         if (masses.length > 2) {
             masses[2].mass = this.value;
             painter.draw();
@@ -32,7 +30,8 @@ window.onload = function() {
 
     window.addEventListener("click", function(e) {
         if (masses.length < 3) {
-            let mass = new Mass(painter, e.clientX, e.clientY, slider1.value, painterPath);
+            let mass = new Mass(painter, e.clientX, e.clientY, sliders[masses.length].value, painterPath);
+            sliders[masses.length].parentNode.style.display = "block";
             masses.push(mass);
             painter.addObject(mass);
             painter.draw();
