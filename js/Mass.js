eb1107f644e8313a7538a88da852a3183a0d0733
luckymurphydevelopment <luckymurphydevelopment@gmail.com>
2020-11-21 15:26:10 -0700

    stuff

--- js/Mass.js
+++ js/Mass.js
@@ -3,7 +3,9 @@ class Mass {
         this.x = x;
         this.y = y;
         this.mass = mass;
+        this.radius = mass * 1.5;
         this.painter = painter;
+        
         this.path = new Path([this.x, this.y]);
         painterPath.addObject(this.path);
         this.painter.addObject(painterPath);
