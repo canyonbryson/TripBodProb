d54165b75850ecef2e9fe65222ee2b53ba91ee68
canyonbryson <canyonbryson@yahoo.com>
2020-11-21 16:27:34 -0700

    update position

--- js/Animate.js
+++ js/Animate.js
@@ -17,8 +17,14 @@ function updateVectors(masses) {
     }
 }
 
-function updatePosition(masses, timestep) {
+function updatePosition(masses, t) {
     //get vector(direction, magnitude)
     //update x, y based on vector and timestep
+    for (let i = 0; i < masses.length; i++){
+        let m = masses[i];
+        let v = m.vector;
+        m.x += Math.cos(v[0]) * v[1] * t;
+        m.y += Math.sin(v[0]) * v[1] * t;
+    }
 }
 
