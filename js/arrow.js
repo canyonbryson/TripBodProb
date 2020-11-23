058f99259b0bd5241c6bfcb76801eecfa9557c76
canyonbryson <canyonbryson@yahoo.com>
2020-11-23 15:24:15 -0700

    arrows

--- js/arrow.js
+++ js/arrow.js
@@ -6,11 +6,13 @@ class Arrow {
     draw(ctx) {
         let lengthX = this.mass.vector.get_x_component() / 100;
         let lengthY = this.mass.vector.get_y_component() / 100;
-        ctx.strokeStyle = "orange";
+        ctx.strokeStyle = "#C0C0C0";
+        ctx.lineWidth = 3;
         ctx.beginPath();
         ctx.moveTo(this.mass.x, this.mass.y);
         ctx.lineTo(this.mass.x + lengthX, this.mass.y + lengthY);
         ctx.stroke();
         ctx.closePath();
+        ctx.lineWidth = 1;
     }
 }
