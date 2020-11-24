98762a0a8bcea8d3228a8e7c59948bd2d8842eb8
canyonbryson <canyonbryson@yahoo.com>
2020-11-23 21:22:03 -0700

    a

--- js/Mass.js
+++ js/Mass.js
@@ -17,7 +17,7 @@ class Mass {
     }
 
     nextIcon() {
-        if (this.icon == 2) {
+        if (this.icon == 3) {
             this.icon = 0;
         } else {
             this.icon++;
@@ -36,7 +36,7 @@ class Mass {
                 ctx.fill();
                 ctx.closePath();
                 break;
-            case 1: 
+            case 1: // orb
                 var grd = ctx.createRadialGradient(this.x, this.y, 1, this.x, this.y, this.radius);
                 grd.addColorStop(1, "rgba(0,0,0,0)"); // transparent
                 grd.addColorStop(0.91, this.color);
@@ -46,7 +46,16 @@ class Mass {
                 ctx.fillStyle = grd;
                 ctx.fillRect(this.x - this.radius, this.y - this.radius, this.x + this.radius * 2, this.y + this.radius * 2);
                 break;
-            case 2:
+            case 2: // black hole
+                var grd = ctx.createRadialGradient(this.x, this.y, 1, this.x, this.y, this.radius);
+                grd.addColorStop(1, "rgba(0,0,0,0)"); // transparent
+                grd.addColorStop(0.91, this.color);
+                grd.addColorStop(0.9, "black");
+                grd.addColorStop(0, "black");
+                ctx.fillStyle = grd;
+                ctx.fillRect(this.x - this.radius, this.y - this.radius, this.x + this.radius * 2, this.y + this.radius * 2);
+                break;
+            case 3:
                 ctx.save();
                 ctx.translate(this.x, this.y);
                 ctx.rotate(this.vector.direction);
